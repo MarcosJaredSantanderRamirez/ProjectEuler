8cca01ca9cc427d796c0db6518e0b5a05b0decf9
zero_mercy <francosconysantor@gmail.com>
2024-04-26 17:17:06 -0600

    26042024

--- C#/ProjectEuler/EvenFibonacci/EvenFibonacci/Program.cs
+++ C#/ProjectEuler/EvenFibonacci/EvenFibonacci/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Fibonacci{
+    static class Problema{
+        static void Main(String[] args){
+            int[] valores = new int[32];
+            int suma = 0;
+
+            valores[0] = 1;
+            valores[1] = 2;
+            Console.WriteLine(valores[0]);
+            Console.WriteLine(valores[1]);
+
+            for (int i = 2; i < valores.Length; i++) {
+                    valores[i] = valores[i - 1] + valores[i - 2];
+                    if (valores[i] < 4000000) {
+                        Console.WriteLine(valores[i]);
+                    }
+                    else if (valores[i] > 4000000) {
+                        Console.WriteLine("Los valores exceden 400000");
+                        valores[i] = 0;
+                        for (int j = 0; j < valores.Length; j++) {
+                            if (valores[j] % 2 == 0){
+                                suma += valores[j];
+                            }
+                         }
+                        Console.WriteLine("La suma de pares es " + suma);
+                        break;
+                    }
+            }
+        }
+    }
+}
--- C#/ProjectEuler/EvenFibonacci/EvenFibonacci/obj/Debug/net8.0/EvenFibonacci.AssemblyInfo.cs
+++ C#/ProjectEuler/EvenFibonacci/EvenFibonacci/obj/Debug/net8.0/EvenFibonacci.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvenFibonacci")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60aacc184a7019596399a1cad781412005ecb9ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2032169d7da9058d7155d967e6e627ad7c06b022")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvenFibonacci")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvenFibonacci")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

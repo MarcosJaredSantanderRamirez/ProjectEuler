3790da9647832341abdfd8a972a978cbf022ef06
zero_mercy <francosconysantor@gmail.com>
2024-04-26 18:20:44 -0600

    Solucionado

--- C#/ProjectEuler/LargestPrimeFactor/LargestPrimeFactor.cs
+++ C#/ProjectEuler/LargestPrimeFactor/LargestPrimeFactor.cs
@@ -1,24 +1,26 @@
-﻿namespace LargestPrimerFactor{
-    public class FactoresPrimos{
-        static void Main(string[] args){
-            long valor = 600851475143;
-            int divisor = 1;
-            int[] primos = new int[50];
-            int c = 0;
-            while(valor!=1){
-                divisor++;
-                if(valor%divisor == 0){
-                    primos[c] = divisor;
-                    valor = valor / divisor;
-                    c++;
-                }
-            }
-            foreach (int i in primos)
+﻿public class FactoresPrimos
+{
+    static void Main(string[] args)
+    {
+        long valor = 600851475143;
+        int divisor = 1;
+        int[] primos = new int[50];
+        int c = 0;
+        while (valor != 1)
+        {
+            divisor++;
+            if (valor % divisor == 0)
             {
-                if(i == 0){continue;}
-                Console.WriteLine(i);
-                
+                primos[c] = divisor;
+                valor = valor / divisor;
+                c++;
             }
+        }
+        foreach (int i in primos)
+        {
+            if (i == 0) { continue; }
+            Console.WriteLine(i);
+
         }
     }
 }
